a1998ae45ad681fe1b391e16dc73a2048055c74f
angelo <gelo.guimba@gmail.com>
2022-12-21 23:05:13 +0800

    Adjust access point

--- src/public/index.php
+++ src/public/index.php
@@ -7,4 +7,14 @@ include __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 
-phpinfo();
+$config = new App\Config($_ENV);
+
+$router = (new App\Core\Router())
+        ->get('/', [App\Controllers\TestController::class, 'index']);
+
+(new App\Core\App(
+    ['uri' => $_SERVER['REQUEST_URI'], 'method' => $_SERVER['REQUEST_METHOD']],
+    $router, 
+    $config
+))
+->run();
