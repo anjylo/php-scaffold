2e9df657757f3b4d0e3a5d55b0f9ea1596bb9738
angelo <gelo.guimba@gmail.com>
2022-12-21 23:04:03 +0800

    Add model functionality

---
+++ src/app/Core/Model.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core;
+
+use App\Core\App;
+
+class Model
+{
+    public static function create(array $data)
+    {
+        $class = new static;
+
+        $table = $class->table ?? strtolower((new \ReflectionClass($class))->getShortName());
+        
+        $columns = implode(', ', array_keys($data));
+        $placeholders = rtrim(str_repeat('?, ', count($data)), ', ');
+
+        $query = "INSERT INTO $table ($columns) VALUES ($placeholders)";
+        
+        $connection = App::db();
+
+        try {
+            $stmt = $connection->prepare($query);
+            $stmt->execute($data);
+
+            return true;
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage());
+        }
+    }
+
+    public static function insert(array $data)
+    {
+        $class = new static;
+
+        $table = $class->table ?? strtolower((new \ReflectionClass($class))->getShortName());
+        
+        $insert_values = [];
+
+        $columns = implode(', ', array_keys($data[0]));
+
+        foreach ($data as $d) {
+            $placeholders[] = '(' . rtrim(str_repeat('?, ', count($d)), ', ') . ')';
+            $insert_values = array_merge($insert_values ?? [], array_values($d));
+        }
+
+        $query = "INSERT INTO $table ($columns) VALUES " . implode(', ', $placeholders);
+
+        $connection = App::db();
+
+        try {
+            if (!$connection->beginTransaction()) {
+                $connection->beginTransaction();
+            }
+
+            $stmt = $connection->prepare($query);
+            $stmt->execute($insert_values);
+
+            $connection->commit();
+
+            return true;
+        } catch (\PDOException $e) {
+            if (!$connection->inTransaction()) {
+                $connection->rollBack();
+            }
+
+            throw new \PDOException($e->getMessage());
+        }
+    }
+
+}
